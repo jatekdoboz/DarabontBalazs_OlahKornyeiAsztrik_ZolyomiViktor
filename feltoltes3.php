ee05a069563c191ac8de4bedfa28b0e28c064f4e
jatekdoboz <104008835+jatekdoboz@users.noreply.github.com>
2022-04-19 17:00:12 +0200

    upload Szerver

---
+++ feltoltes3.php
@@ -0,0 +1,23 @@
+<?php
+
+
+ if($_SERVER['REQUEST_METHOD'] == 'POST')
+ {
+ 
+ $ImageData = $_POST['utvonal'];
+ 
+ $ImageName = $_POST['kepnev'];
+ 
+ $ImagePath = "image/$ImageName";
+ 
+ $ServerURL = "https://localhost/$ImagePath";
+
+ file_put_contents($ImagePath,base64_decode($ImageData));
+
+ echo $ImageName;
+ echo "Sikeres feltoltes";
+ }else{
+ echo "Nem sikerult";
+ }
+
+?>
